ab28feec345a8a8b4669464c4a4aab61aba63f8b
Moonsight91 <aidanali65@gmail.com>
2024-10-03 17:59:34 -0400

    Cheeky

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const App = () => {
 
     return (
         <div className="app">
-            <h1>MovieLand</h1>
+            <h1>FilmPulse</h1>
 
            <div className="search">
         <input placeholder="Search for Movies"
